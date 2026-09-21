e200f2876548e2a6eb92ebca91d0d4e6efa192f4
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-09-21 09:15:22 +0700

    refactor(terraform): remove dev resource definitions

--- terraform-openstack/environments/dev/keypair.tf
+++
@@ -1,5 +0,0 @@
-# Import the existing local public key into OpenStack for SSH access.
-resource "openstack_compute_keypair_v2" "cluster" {
-  name       = var.keypair_name
-  public_key = file(pathexpand(var.public_key_path))
-}
