9d61b07fefb02718cdde90813b33ed5e546435e8
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-09-21 09:28:19 +0700

    refactor(terraform): move dev inventory to tfvars

--- terraform-openstack/environments/dev/variables.tf
+++ terraform-openstack/environments/dev/variables.tf
@@ -1,37 +1,31 @@
 variable "network_name" {
   description = "Existing external OpenStack network name."
   type        = string
-  default     = "Public_Net"
 }
 
 variable "network_id" {
   description = "Expected ID of the existing Public_Net network."
   type        = string
-  default     = "c3455e8f-ea16-4f5d-ad5e-5c4292015a0d"
 }
 
 variable "security_group_name" {
   description = "Name for the shared Kubernetes security group."
   type        = string
-  default     = "k8s-cluster-sg"
 }
 
 variable "keypair_name" {
   description = "OpenStack keypair name to import."
   type        = string
-  default     = "kltn-autoscaling"
 }
 
 variable "public_key_path" {
   description = "Path to the SSH public key imported into OpenStack."
   type        = string
-  default     = "~/.ssh/kltn_autoscaling.pub"
 }
 
 variable "image_id" {
   description = "Ubuntu 22.04 Glance image ID used to create boot volumes."
   type        = string
-  default     = "a04bdcd9-40e7-40ba-bd21-7d410965e3a3"
 }
 
 variable "nodes" {
@@ -43,10 +37,4 @@ variable "nodes" {
     flavor_name    = string
     volume_size_gb = number
   }))
-
-  default = [
-    { name = "node-app", role = "app", flavor_id = "00-0000-0008-04", flavor_name = "gp.large-4c", volume_size_gb = 120 },
-    { name = "node-observability", role = "observability", flavor_id = "00-0000-0002-01", flavor_name = "gp.small-1c", volume_size_gb = 50 },
-    { name = "node-loadgen", role = "loadgen", flavor_id = "00-0000-0002-01", flavor_name = "gp.small-1c", volume_size_gb = 30 },
-  ]
 }
